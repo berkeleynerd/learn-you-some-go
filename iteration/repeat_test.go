201b266d9a74a373b58ded2e0c61448ff772ed92
berkeleynerd <31438281+berkeleynerd@users.noreply.github.com>
2021-11-09 10:52:19 +0000

    Add example function call

--- iteration/repeat_test.go
+++ iteration/repeat_test.go
@@ -1,11 +1,18 @@
 package iteration
 
+import "fmt"
 import "testing"
 
 func BenchmarkRepeat(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		Repeat("a", 5)
 	}
+}
+
+func ExampleRepeat() {
+	repeated := Repeat(".", 4)
+	fmt.Println(repeated)
+	// Output: ....
 }
 
 func TestRepeat(t *testing.T) {
