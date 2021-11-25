f763edfd6c6c4949369ed500475a9d975677c347
berkeleynerd <31438281+berkeleynerd@users.noreply.github.com>
2021-11-25 08:06:45 +0000

    Add Area function for circles

--- structs-methods-and-interfaces/shapes.go
+++ structs-methods-and-interfaces/shapes.go
@@ -1,12 +1,22 @@
 package shapes
 
+import "math"
+
+type Circle struct {
+	Radius float64
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
+}
+
 type Rectangle struct {
 	Width  float64
 	Height float64
 }
 
-func Area(rectangle Rectangle) float64 {
-	return rectangle.Width * rectangle.Height
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
 }
 
 func Perimeter(rectangle Rectangle) float64 {
