2f850d299848d4f860a6717279f8556470114246
Rebecca <31438281+berkeleynerd@users.noreply.github.com>
2022-01-04 13:42:50 +0000

    Refactor to provide caller with meaningful error message

--- pointers-and-errors/wallet_test.go
+++ pointers-and-errors/wallet_test.go
@@ -17,6 +17,17 @@ func TestWallet(t *testing.T) {
 
 	}
 
+	assertError := func(t testing.TB, got error, want string) {
+		t.Helper()
+		if got == nil {
+			t.Fatal("wanted an error but did not get one")
+		}
+
+		if got.Error() != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+	}
+
 	t.Run("Deposit", func(t *testing.T) {
 		wallet := Wallet{}
 		wallet.Deposit(Bitcoin(10))
@@ -35,10 +46,7 @@ func TestWallet(t *testing.T) {
 		err := wallet.Withdraw(Bitcoin(100))
 
 		assertBalance(t, wallet, startingBalance)
-
-		if err == nil {
-			t.Errorf("wanted an error but didn't get one")
-		}
+		assertError(t, err, "cannot withdraw, insufficient funds")
 	})
 
 }
