94f6873c99e7ddf9b8307eaf97aac02f6a3ac8b9
Rebecca <31438281+berkeleynerd@users.noreply.github.com>
2022-01-13 08:56:09 +0000

    a small addendum

---
+++ dependency-injection/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"greet"
+	"io"
+	"log"
+	"net/http"
+)
+
+func MyGreeterHandler(w http.ResponseWriter, r *http.Request) {
+	Greet(w, "world")
+}
+
+func main() {
+	log.Fatal(http.ListenAndServe(":5000", http.HandlerFunc(MyGreeterHandler)))
+}
