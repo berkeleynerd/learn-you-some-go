539f62c4f709ccfb4d9c183446e265911de9b6af
Rebecca <31438281+berkeleynerd@users.noreply.github.com>
2022-03-30 16:36:18 +0000

    Implement test ensuring cancellations are handled correctly

--- contexts/contexts.go
+++ contexts/contexts.go
@@ -1,30 +1,23 @@
 package contexts
 
 import (
+	"context"
 	"fmt"
 	"net/http"
 )
 
 func Server(store Store) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		ctx := r.Context()
+		data, err := store.Fetch(r.Context())
 
-		data := make(chan string, 1)
-
-		go func() {
-			data <- store.Fetch()
-		}()
-
-		select {
-		case d := <-data:
-			fmt.Fprint(w, d)
-		case <-ctx.Done():
-			store.Cancel()
+		if err != nil {
+			return // todo: log error or take other appropriate action
 		}
+
+		fmt.Fprint(w, data)
 	}
 }
 
 type Store interface {
-	Fetch() string
-	Cancel()
+	Fetch(ctx context.Context) (string, error)
 }
