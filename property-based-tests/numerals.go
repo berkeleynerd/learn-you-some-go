411e33a40fd0ec42fc088296ce7f263042618161
Rebecca <31438281+berkeleynerd@users.noreply.github.com>
2022-04-07 11:09:49 +0000

    Handle all remaining Roman Numerals

--- property-based-tests/numerals.go
+++ property-based-tests/numerals.go
@@ -1,6 +1,8 @@
 package roman
 
-import "strings"
+import (
+	"strings"
+)
 
 type Numeral struct {
 	Value  int
@@ -8,6 +10,14 @@ type Numeral struct {
 }
 
 var allNumerals = []Numeral{
+	{1000, "M"},
+	{900, "CM"},
+	{500, "D"},
+	{400, "CD"},
+	{100, "C"},
+	{90, "XC"},
+	{50, "L"},
+	{40, "XL"},
 	{10, "X"},
 	{9, "IX"},
 	{5, "V"},
